b9a242754ee2561f6e0a23446758e0fd484be728
Destroy <emptycsgo@mail.ru>
2024-10-08 11:05:08 +0300

    create ServerVersionUtil class

---
+++ src/main/java/ru/spigotmc/destroy/primeseller/util/ServerVersionUtil.java
@@ -0,0 +1,45 @@
+package ru.spigotmc.destroy.primeseller.util;
+
+import org.bukkit.Bukkit;
+
+public class ServerVersionUtil {
+    private static String getVersion() {
+        return Bukkit.getBukkitVersion()
+                .replace("-", "")
+                .replace("R0.1", "")
+                .replace("SNAPSHOT", "");
+    }
+
+    public static ServerVersion getServerVersion() {
+        String version = getVersion();
+        String[] parts = version.split("\\.");
+        int major = Integer.parseInt(parts[0]);
+        int minor = Integer.parseInt(parts[1]);
+        int patch = Integer.parseInt(parts[2]);
+        return new ServerVersion(major,minor,patch);
+    }
+
+    public static class ServerVersion {
+        private final int major;
+        private final int minor;
+        private final int patch;
+
+        public ServerVersion(int major, int minor, int patch) {
+            this.major = major;
+            this.minor = minor;
+            this.patch = patch;
+        }
+
+        public int getMajor() {
+            return major;
+        }
+
+        public int getMinor() {
+            return minor;
+        }
+
+        public int getPatch() {
+            return patch;
+        }
+    }
+}
