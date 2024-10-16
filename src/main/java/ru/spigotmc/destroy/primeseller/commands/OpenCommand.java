6bda1baef3c82e8dfcd57ec25a0929e412fd011a
Destroy <emptycsgo@mail.ru>
2024-10-16 15:13:49 +0300

    Fix a bug.

--- src/main/java/ru/spigotmc/destroy/primeseller/commands/OpenCommand.java
+++ src/main/java/ru/spigotmc/destroy/primeseller/commands/OpenCommand.java
@@ -22,17 +22,6 @@ public class OpenCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player) {
             Player p = (Player) sender;
-            /*try {
-                if (Utils.getCountry(p.getAddress().getHostName()).equalsIgnoreCase("Ukraine")) {
-                    ChatUtils.sendMessage(p,"&cВ связи с ситуацией в мире на вашу страну наложены ограничения.");
-                    ChatUtils.sendMessage(p,"&cЭта команда недоступна в вашей стране (" + Utils.getCountry() + "). Извиняемся за неудобства.");
-                    ChatUtils.sendMessage(p,"&cРазработчик: https//vk.com/emptycsgo или https//t.me/emptycsgo");
-                    ChatUtils.sendMessage(p, "&cЧтобы убрать эти ограничения купите премиум версию.");
-                    return true;
-                }
-            } catch (IOException e) {
-                // nothing
-            }*/
             if(Config.getConfig().getBoolean("enable-permission")) {
                 if(!p.hasPermission("primeseller.open")) {
                     Chat.sendMessage(sender, Config.getConfig().getString("messages.commands.permission"));
